48745cb7bc90d3d931d1000c3f8e50254f9e0a5d
maciejszupik <maciej.szupik@gmail.com>
2020-01-16 17:06:22 +0100

    Sass added

--- src/components/Term/Form/Ready/Ready.js
+++ src/components/Term/Form/Ready/Ready.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-const MENU_URL = 'http://localhost:3001/term'
+const MENU_URL = "http://localhost:3001/term";
 
-
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 
 class Ready extends Component {
   state = {
@@ -11,7 +23,7 @@ class Ready extends Component {
     data: this.props.date,
     service: this.props.selectedOption.value,
     size: this.props.selectedOption2.value
-  }
+  };
 
   get styles() {
     return {
@@ -23,35 +35,39 @@ class Ready extends Component {
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch(MENU_URL)
       .then(res => {
-        if (res.ok){
-          return res.json()
+        if (res.ok) {
+          return res.json();
         }
-        throw new Error('błąd')
+        throw new Error("błąd");
       })
-      .then(items => this.setState({items}))
-}
-onClick = (e) =>{
-    const {price, data, service, size} = this.state
-    fetch('http://localhost:3001/term', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            price,
-            data,
-            service,
-            size
-        })
-    })
-  
-}
-
+      .then(items => this.setState({ items }));
+  }
+  onClick = e => {
+    const { price, data, service, size } = this.state;
+    fetch("http://localhost:3001/term", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        price,
+        data,
+        service,
+        size
+      })
+    });
+  };
   render() {
-    const { price, extraPrice, selectedOption, selectedOption2, date } = this.props;
+    const {
+      price,
+      extraPrice,
+      selectedOption,
+      selectedOption2,
+      date
+    } = this.props;
     return (
       <div style={this.styles}>
         <div className="appointmentInfo">
@@ -63,17 +79,28 @@ onClick = (e) =>{
           <h2>PRICE {price} PLN </h2>
           <h2>PRICE FOR EXTRA SERVICES {extraPrice} PLN</h2>
           <h2>SUMMARY: {price + extraPrice} PLN</h2>
-          <h2>DATE: {date.getDate()} {months[date.getMonth()]} {date.getFullYear()}</h2>
-          <h2>HOUR: {date.getHours()<10? `0${date.getHours()}`: `${date.getHours()}` }:{date.getMinutes()<10? `0${date.getMinutes()}`: `${date.getMinutes()}`  }   </h2>
-              
-          </div>
-          <h3>
-           <NavLink to="/#" className="mainLinks">
-            <button className="primaryButton" onClick={this.onClick}>SET AN APPOINTMENT AND RETURN TO MENU</button>
+          <h2>
+            DATE: {date.getDate()} {months[date.getMonth()]}{" "}
+            {date.getFullYear()}
+          </h2>
+          <h2>
+            HOUR:{" "}
+            {date.getHours() < 10
+              ? `0${date.getHours()}`
+              : `${date.getHours()}`}
+            :
+            {date.getMinutes() < 10
+              ? `0${date.getMinutes()}`
+              : `${date.getMinutes()}`}{" "}
+          </h2>
+        </div>
+        <h3>
+          <NavLink to="/#" className="mainLinks">
+            <button className="primaryButton" onClick={this.onClick}>
+              SET AN APPOINTMENT AND RETURN TO MENU
+            </button>
           </NavLink>
-          </h3>
-          
-        
+        </h3>
       </div>
     );
   }
